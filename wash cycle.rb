7854f73c10d86960d6cccf4e67bdaf8261ea68ad
scsknghtblde <scsknghtblde@gmail.com>
2016-08-10 12:42:03 -0400

    Added conditional

--- wash cycle.rb
+++ wash cycle.rb
@@ -1,21 +1,27 @@
 # Welcome to Sonic Pi v2.10
 def wear(smell)
-  sample (smell)
-  sleep (1)
+  sample(smell)
+  sleep(1)
 end
 
-def wash (temperature)
-  play (temperature)
-  sleep (1)
+def wash(temperature)
+  clean=[true, false].choose
+  if clean ==true
+    play(70)
+    sleep(1)
+  else
+    play (10)
+    sleep(1)
+  end
 end
 
-def dry (setting)
-  sample (setting)
-  sleep (1)
+def dry(setting)
+  sample(setting)
+  sleep(1)
 end
 
 3.times do
-  wear (:drum_heavy_kick)
-  wash (70)
-  dry (:drum_cymbal_pedal)
+  wear(:drum_heavy_kick)
+  wash(70)
+  dry(:drum_cymbal_pedal)
 end
